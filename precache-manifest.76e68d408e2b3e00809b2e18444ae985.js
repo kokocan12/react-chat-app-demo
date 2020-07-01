c143b59d95561fc75fb106e1e779f4feee85a10a
kokocan12 <49009864+kokocan12@users.noreply.github.com>
2020-07-01 18:26:12 +0900

    Initial commit

---
+++ precache-manifest.76e68d408e2b3e00809b2e18444ae985.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21264007666dba39e5b2d6995e970f2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bf22955cda8ccf3470b",
+    "url": "/static/js/2.c4f9dda0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c4f9dda0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7cf35f9a7235b36e6b",
+    "url": "/static/js/main.c6b94723.chunk.js"
+  },
+  {
+    "revision": "3cf8ae71a09948a08a2e",
+    "url": "/static/js/runtime-main.4857cacc.js"
+  }
+]);
